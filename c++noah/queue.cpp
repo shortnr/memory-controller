4e91cd59ad23c50b38924009c15072dcc12caf27
knowapage <hoan125@gmail.com>
2020-11-15 16:04:15 -0800

    fixed the main issue. Added time in queue. Need output

---
+++ c++noah/queue.cpp
@@ -0,0 +1,98 @@
+#include "queue.h"
+
+//this is the constructor
+queue::queue()
+{
+    head = NULL;
+}
+
+//this is the destructor
+queue::~queue()
+{
+    if(remove_all(head));
+}
+
+bool queue::remove_all(node * & head)
+{
+    if(!head) return 0;
+    bool value = remove_all(head->next);
+    delete head;
+    head = NULL;
+    return value;
+}
+
+int queue::size()
+{
+    int size = count;
+    return size;
+}
+
+void queue::update_time()
+{
+    node * current = head;
+    while(current)
+    {
+        ++current->q_time;
+        current = current->next;
+    }
+}
+
+
+void queue::add(int request_time, int time_diff, int cmd, addmap & temp)
+{
+    if(head == NULL)
+    {
+        ++count;
+        head = new node;
+	    head->request_time = request_time;
+        head->q_time = time_diff; 
+	    head->cmd = cmd;
+        head->row = temp.row;
+        head->hcol = temp.hcol;
+        head->bg = temp.bg;
+        head->bank = temp.bank;
+        head->lcol = temp.lcol;
+	    head->next = NULL;
+    }
+    else
+    {
+        node * current = head;
+        ++count;
+        while(current->next != NULL){
+            current = current->next;
+        }
+    	current->next = new node;
+    	current = current->next;
+	    current->request_time = request_time;
+        current->q_time = time_diff; 
+	    current->cmd = cmd;
+        current->row = temp.row;
+        current->hcol = temp.hcol;
+        current->bg = temp.bg;
+        current->bank = temp.bank;
+        current->lcol = temp.lcol;
+    	current->next = NULL;
+    }
+}
+
+void queue::display_all()
+{
+    node * temp = head;
+    char cmd[64];           //Used in Switch/Case to determine the commands name (READ, WRITE, FETCH)
+    cout << "\nBank Group " << temp->bg << "'s Queue contains:\n";
+    while(temp != NULL)
+    {
+        //Determine the commands name (i.e. op=0 -> Data READ)
+        switch(temp->cmd){
+            case 0: strcpy(cmd,"Data READ");
+                    break;
+            case 1: strcpy(cmd,"Data WRITE");
+                    break;
+            case 2: strcpy(cmd,"Instruction FETCH");
+                    break;
+        }
+        printf("request_time: %d CPU clocks\nTime in Queue: %dDIMM clocks\nOperation: %d (%s)\n",temp->request_time, temp->q_time, temp->cmd, cmd);
+        printf("row: %d | Hi_col: %d | BG: %d | Bank: %d | Low_col: %d\n\n",temp->row, temp->hcol, temp->bg, temp->bank, temp->lcol);
+        temp = temp->next;
+    }
+}
---
+++ c++noah/queue.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+using namespace std;
+
+/*
+ * Struct addmap used to hold the mapped vals from a hex address.
+ * Struct type used by function to return an addmap struct with mapped vals.
+*/
+struct addmap{
+	int row, hcol, bg, bank, lcol;
+};
+
+struct node
+{
+	int request_time, cmd, row, hcol, bg, bank, lcol; //from CPU
+	int q_time; //
+	node * next = NULL;
+};
+
+class queue
+{
+public:
+	queue();
+	void add(int request_time, int time_diff, int cmd, addmap & temp);
+	void update_time();
+	int size();
+	void display_all();
+	~queue();
+
+private:
+	node * head;
+	int count = 0;
+	//int time_till_avail = 0;
+	bool remove_all(node * & head);
+};
